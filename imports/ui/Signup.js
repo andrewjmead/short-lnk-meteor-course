1b34ce04aacac5d172fbfabae2410f3670952afc
Andrew Mead <andrew@mead.io>
2017-01-18 09:41:44 -0500

    Update signup language

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -41,7 +41,7 @@ export default class Signup extends React.Component {
             <button className="button">Create Account</button>
           </form>
 
-          <Link to="/">Already have an account?</Link>
+          <Link to="/">Have an account?</Link>
         </div>
       </div>
     );
